47517d2b2f5e75aaf3b3c64e029c0a7bc8479cd5
Sheng-Fu Chang <csf@csftech.net>
2018-05-17 10:26:30 +0800

    Add Spark response.header example code

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -4,7 +4,12 @@ package app
 import spark.kotlin.*
 
 fun main(args: Array<String>) {
+    staticFiles.header("server", "Jetty")
     staticFiles.location("/html/dist/html")
+
+    before("/*") {
+        response.header("server", "Jetty")
+    }
 
     get("/hello") {
         type(contentType = "application/json")
